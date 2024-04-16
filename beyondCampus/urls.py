2db4f0c5b5186df40d999e30d77cab6ff22dcb24
danile981199 <63995138+danile981199@users.noreply.github.com>
2024-04-16 18:48:48 -0400

    Update urls.py

--- beyondCampus/urls.py
+++ beyondCampus/urls.py
@@ -9,3 +9,17 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("logout/", views.logout_user, name="logout"),
 ]
+
+urlpatterns = [
+    path('properties/<int:property_id>/add_favourite/', add_to_favourites, name='add_to_favourites'),
+    path('properties/<int:property_id>/favourites/', show_favourites, name='show_favourites'),
+    path('properties/<int:property_id>/myfavourites/', my_favourites, name='my_favourites'),
+    # Make sure you have views and URLs set for 'property_details' and 'listing'
+]
+urlpatterns = [
+    path('properties/<int:property_id>/report/', report_issue, name='report_issue'),
+]
+
+urlpatterns = [
+    path('properties/<int:property_id>/reviews/', property_reviews, name='property_reviews'),
+]
