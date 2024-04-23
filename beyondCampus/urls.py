434ae710378c6e52c798008b94c81302cfb86063
danile981199 <63995138+danile981199@users.noreply.github.com>
2024-04-23 14:03:53 -0400

    Update urls.py

--- beyondCampus/urls.py
+++ beyondCampus/urls.py
@@ -8,20 +8,21 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("profile/", views.profile, name="profile"),
     path("logout/", views.logout_user, name="logout"),
-    path("dashboard/", views.myhouse_dashboard, name='myhouse_dashboard'),
+
     path("applications/", views.show_applications, name="applications"),
     path('create-listing/', views.create_listing, name='create_listing'),
+    path('apply/<int:listing_id>/', apply_to_listing, name='apply_to_listing'),
+
+    path('',views.student_dashboard, name='student_dashboard'),
+    path('',views.my_favourites, name='my_favourites'),
+    path('',views.myhouse_dashboard, name='myhouse_dashboard'),
     path("maintenance/<int:property_id>/", views.request_maintenance, name='request_maintenance'),
     path("insurance/<int:property_id>/", views.insurance_coverage, name='insurance_coverage'),
     path("utility/<int:property_id>/", views.utility_provider, name='utility_provider'),
-    path('apply/<int:listing_id>/', apply_to_listing, name='apply_to_listing'),]
 
-urlpatterns = [
-    path('properties/<int:property_id>/add_favourite/', add_to_favourites, name='add_to_favourites'),
-    path('properties/<int:property_id>/favourites/', show_favourites, name='show_favourites'),
-    path('properties/<int:property_id>/myfavourites/', my_favourites, name='my_favourites'),
-    # Make sure you have views and URLs set for 'property_details' and 'listing'
+
 ]
+
 urlpatterns = [
     path('properties/<int:property_id>/report/', report_issue, name='report_issue'),
 ]
