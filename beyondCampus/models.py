f79113b745dadfca21f0d1305f8a8e8f5125d5cb
rmkuturu <164095384+rmkuturu@users.noreply.github.com>
2024-04-23 17:59:58 -0400

    Update models.py

--- beyondCampus/models.py
+++ beyondCampus/models.py
@@ -87,6 +87,8 @@ class Apply(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     application_num = models.IntegerField()
+    preferences = models.CharField(max_length=255, blank=True, null=True)
+    additional_info = models.TextField(blank=True, null=True) 
 
     def __str__(self):
         return self.application_num
