0bcf91ce18f1eb8883ec773abdbfcd5bc9985c02
rmkuturu <164095384+rmkuturu@users.noreply.github.com>
2024-04-23 01:31:03 -0400

    Update urls.py

--- beyondCampus/urls.py
+++ beyondCampus/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('create-listing/', views.create_listing, name='create_listing'),
     path("maintenance/<int:property_id>/", views.request_maintenance, name='request_maintenance'),
     path("insurance/<int:property_id>/", views.insurance_coverage, name='insurance_coverage'),
-    path("utility/<int:property_id>/", views.utility_provider, name='utility_provider'),]
+    path("utility/<int:property_id>/", views.utility_provider, name='utility_provider')
+    path("submit-application/", views.application_submission, name='submit_application'),]
 
 urlpatterns = [
     path('properties/<int:property_id>/add_favourite/', add_to_favourites, name='add_to_favourites'),
